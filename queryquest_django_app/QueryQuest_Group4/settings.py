ff75825322e844a260ff45d5dcda38d67b3392bd
joyxxi <joyx29580@gmail.com>
2025-03-25 13:21:19 -0700

    Basic APIs for Problems
        # GET    /api/problems/           -> list all problems
        # POST   /api/problems/           -> create a problem
        # GET    /api/problems/{pk}/      -> retrieve a problem by id
        # PUT    /api/problems/{pk}/      -> update a problem
        # DELETE /api/problems/{pk}/      -> delete a problem
        # GET    /api/problems/unit/<unit_id>/ -> custom action for problems in a single unit

--- queryquest_django_app/QueryQuest_Group4/settings.py
+++ queryquest_django_app/QueryQuest_Group4/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Django REST framework
+    'rest_framework',
+
+    # Apps
     'submissions',
 ]
 
@@ -88,14 +93,14 @@ if os.getenv('GAE_APPLICATION', None):
             'HOST': '/cloudsql/db-group4-451519:us-west1:neu-5200-group4', 
             'USER': 'yihan',
             'PASSWORD': 'yihan123!',
-            'NAME': 'test',
+            'NAME': 'queryquest',
         }
     }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'test',
+            'NAME': 'queryquest',
             'USER': 'yihan',
             'PASSWORD': 'yihan123!',
             'HOST': '35.247.120.164',
---
+++ queryquest_django_app/problems/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProblemViewSet
+
+router = DefaultRouter()
+router.register(r'', ProblemViewSet, basename='problem')
+
+urlpatterns = [
+    # This will generate routes like:
+    # GET    /api/problems/           -> list all problems
+    # POST   /api/problems/           -> create a problem
+    # GET    /api/problems/{pk}/      -> retrieve a problem by id
+    # PUT    /api/problems/{pk}/      -> update a problem
+    # DELETE /api/problems/{pk}/      -> delete a problem
+    # GET    /api/problems/unit/<unit_id>/ -> custom action for problems in a single unit
+    path('', include(router.urls)),
+]
