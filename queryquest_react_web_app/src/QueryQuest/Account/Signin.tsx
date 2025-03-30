cfed637f3b7ba7c6e30217e66697b8ecf3b79f1a
codehroll <judyh0665@gmail.com>
2025-03-30 12:21:56 -0700

    update Account routes

--- queryquest_react_web_app/src/QueryQuest/Account/Signin.tsx
+++ queryquest_react_web_app/src/QueryQuest/Account/Signin.tsx
@@ -5,12 +5,12 @@ export default function Signin() {
       <h3>Sign in</h3>
       <input id="wd-username" placeholder="username" /> <br />
       <input id="wd-password" placeholder="password" type="password" /> <br />
-      <Link id="wd-signin-btn" to="/Queryquest/Account">
+      <Link id="wd-signin-btn" to="/QueryQuest/Account">
         {" "}
         Sign in{" "}
       </Link>{" "}
       <br />
-      <Link id="wd-signup-link" to="/Queryquest/Signup">
+      <Link id="wd-signup-link" to="/QueryQuest/Signup">
         Sign up
       </Link>
     </div>
